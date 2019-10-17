bcf701d3712e02b00af38118b4ec53c2332fd258
steoneill <ste.tpe@gmail.com>
2019-10-17 13:03:52 +0100

    Fixes for duration types

--- main.go
+++ main.go
@@ -7,18 +7,16 @@ import (
 	"time"
 )
 
-var startDate duration
-var dateSince duration
-
-var mutex = &async.Mutex{}
+var startDate time.Time
+var dateSince time.Duration
 
 func main() {
 	startDate = time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC)
-	dateSince = time.Since(start)
-	fmt.Println(since)
+	dateSince = time.Since(startDate)
+	fmt.Println(dateSince)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Time since last pizza:", since.String())
+		fmt.Fprintf(w, "Time since last pizza:", dateSince)
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
