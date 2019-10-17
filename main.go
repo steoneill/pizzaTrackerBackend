e92e67a22a7715af22a5fe15832f83443678f793
steoneill <ste.tpe@gmail.com>
2019-10-17 12:59:26 +0100

    changes to add more complexity

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 )
 
+var startDate duration
+var dateSince duration
+
+var mutex = &async.Mutex{}
+
 func main() {
-	start := time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC)
-	since := time.Since(start)
+	startDate = time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC)
+	dateSince = time.Since(start)
 	fmt.Println(since)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Time since last pizza:", since.String())
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
-
-
-
